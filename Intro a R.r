a8565e0f1fdd629e7c4d89e827c4197ecea002ae
EmiliVal <142239164+EmiliVal@users.noreply.github.com>
2024-01-17 19:53:17 -0600

    Add files via upload

---
+++ Intro a R.r
@@ -0,0 +1,72 @@
+install.packages("ggplot2")
+
+1
+
+1+1
+
+x <- 1 
+x
+
+x = 12
+x
+
+x
+
+msg <- "hola"
+msg
+
+2*3
+
+2**3
+
+2/2
+
+x <- c(0.5, 0.6)
+x <- c(TRUE, FALSE)
+x <- c(T, F)
+x <- c("a", "b", "c")
+x <- 9:29
+x <- c(1+0i, 2+4i)
+x
+
+y <- c(1.7, "a")
+y
+
+class(y)
+
+
+
+x <- vector("numeric", length = 10)
+x
+
+y <- c(TRUE, 2)
+paste("Valor de y:")
+y
+paste("clase de y:")
+class(y)
+
+m <- matrix(nrow = 2, ncol = 3)
+m
+
+dim(m)
+
+m <- matrix(1:6, nrow = 2, ncol = 3)
+m
+
+x <- 1:3
+y <- 10:12
+cbind(x,y)
+
+rbind(x,y)
+
+x <- list(1, "a", TRUE, 1 + 4i)
+x
+
+x <- factor(c("yes", "yes", "no", "yes", "no"))
+x
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
+
